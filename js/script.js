25e53ccba7bebc743490b44b1ab628540055b53a
Danny Kim <dkim7474@gmail.com>
2017-01-10 14:19:09 -0500

    cleaned up script.js and commented some code

--- js/script.js
+++ js/script.js
@@ -1,48 +1,4 @@
-// Function to create AJAX request to reddit.com/r/mma
-// Success
-// (function() {
-// 	var button = document.getElementById("btn");
-//
-// 	button.addEventListener("click", function() {
-// 		var theRequest = new XMLHttpRequest();
-// 		theRequest.open('GET', 'https://www.reddit.com/r/MMA.json');
-//
-// 		theRequest.onload = function() {
-// 		// after the onload event occurs, 'responseText' contains complete server response
-// 			if (theRequest.status >= 200 && theRequest.status < 400) {
-// 				console.log("Request was a success");
-// 				var ourData = JSON.parse(theRequest.responseText);
-// 				console.log(ourData.data.children);
-// 				var allChildren = ourData.data.children;
-// 				displayPostTitles(allChildren);
-// 				// this above will get the title of the first post
-// 				// on the reddit/r/mma subreddit
-// 			} else {
-// 				console.log("Something went wrong");
-// 			}
-// 		}
-// 		console.log(theRequest)
-// 		theRequest.send();
-// 	});
-//
-// 	function displayPostTitles(allPosts) {
-// 		var content = document.getElementById("content");
-// 			posts = allPosts;
-// 			postNumber = allPosts.length;
-// 		for (var x = 0; x < 5; x += 1) {
-// 			var divPost = document.createElement("div");
-// 				postLink = document.createElement("a");
-// 			content.appendChild(divPost);
-// 			postLink.href = allPosts[x].data.url;
-// 			postLink.innerHTML = allPosts[x].data.title;
-// 			divPost.appendChild(postLink);
-// 		}
-// 	}
-//
-// })();
-
-
-// Function to create most popular
+// Wrapped in IIFE to make more modular and not to pollute global namespace
 (function() {
 var popularSubs = ["AskReddit", "funny", "todayilearned", "science", "pics", "worldnews", "IAmA", "gaming", "videos", "movies"];
 		subs = document.getElementById("subs");
@@ -51,7 +7,6 @@ var popularSubs = ["AskReddit", "funny", "todayilearned", "science", "pics", "wo
 function showSubs(subArray) {
 
 	for (var i = 0; i < subArray.length; i += 1) {
-		// subs.innerHTML += this[i] + ". "
 		var subLinks = document.createElement("a");
 				subDivs = document.createElement("div");
 				linkBox = document.createElement("div");
@@ -72,16 +27,14 @@ function showSubs(subArray) {
 		subDivs.appendChild(linkBox);
 
 		popRequest(popularSubs[i]);
-		// this popRequest function brings back the top 5 links in the iterations subreddit
-		// can this be changed with recursion?
+		// this popRequest function brings back the top 5 links on each popularSubs iteration pertaining
+		// to that subreddit. Can this be changed with recursion?
 
 	}
 }
 
 // create a function that will send an ajax request to the .json subreddit
 // that will bring back the links and place them in their correct div
-// in the true conditional, it is only bringing back the first child [0] of the request
-// run a for loop inside conditional?
 function popRequest(sub) {
 	var popSubRequest = new XMLHttpRequest();
 	popSubRequest.open('GET', 'https://www.reddit.com/r/' + sub + '.json');
@@ -89,10 +42,13 @@ function popRequest(sub) {
 	popSubRequest.onload = function() {
 		if (popSubRequest.status >= 200 && popSubRequest.status < 400) {
 			var popSubData = JSON.parse(popSubRequest.responseText);
-			// console.log(popSubData);
 					for (var n = 0; n < 5; n += 1 ) {
-						// console.log(popSubData.data.children[n].data.subreddit);
-						// console.log(popSubData.data.children[n].data.title);
+						/*
+						 These were used to see what type of can be fetched from the returned object
+						 console.log(popSubData.data.children[n].data.subreddit);
+						 console.log(popSubData.data.children[n].data.title);
+						 console.log(popSubData.data.children[n].data);
+						*/
 						var linkPost = document.createElement("a");
 								linkDiv = document.createElement("div");
 								findIdDiv = document.getElementById(popSubData.data.children[n].data.subreddit);
@@ -101,8 +57,6 @@ function popRequest(sub) {
 						linkPost.innerHTML = popSubData.data.children[n].data.title;
 						linkPost.href = popSubData.data.children[n].data.url;
 						linkDiv.appendChild(linkPost);
-						// console.log(findIdDiv);
-						console.log(popSubData.data.children[n].data);
 						findIdDiv.appendChild(linkDiv);
 					}
 		} else {
