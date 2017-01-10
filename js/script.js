426d09edb40edec1c85bcbd1906979b37d82379d
Danny Kim <dkim7474@gmail.com>
2017-01-09 19:44:59 -0500

    Links are appended but /r/askreddit and /r/iama are returning errors for .appendChild of null at XHTML onload request

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
 // Function to create AJAX request to reddit.com/r/mma
 // Success
-(function() {
-	var button = document.getElementById("btn");
-
-	button.addEventListener("click", function() {
-		var theRequest = new XMLHttpRequest();
-		theRequest.open('GET', 'https://www.reddit.com/r/MMA.json');
-
-		theRequest.onload = function() {
-		// after the onload event occurs, 'responseText' contains complete server response
-			if (theRequest.status >= 200 && theRequest.status < 400) {
-				console.log("Request was a success");
-				var ourData = JSON.parse(theRequest.responseText);
-				console.log(ourData.data.children);
-				var allChildren = ourData.data.children;
-				displayPostTitles(allChildren);
-				// this above will get the title of the first post
-				// on the reddit/r/mma subreddit
-			} else {
-				console.log("Something went wrong");
-			}
-		}
-		console.log(theRequest)
-		theRequest.send();
-	});
-
-	function displayPostTitles(allPosts) {
-		var content = document.getElementById("content");
-			posts = allPosts;
-			postNumber = allPosts.length;
-		for (var x = 0; x < 5; x += 1) {
-			var divPost = document.createElement("div");
-				postLink = document.createElement("a");
-			content.appendChild(divPost);
-			postLink.href = allPosts[x].data.url;
-			postLink.innerHTML = allPosts[x].data.title;
-			divPost.appendChild(postLink);
-		}
-	}
-
-})();
+// (function() {
+// 	var button = document.getElementById("btn");
+//
+// 	button.addEventListener("click", function() {
+// 		var theRequest = new XMLHttpRequest();
+// 		theRequest.open('GET', 'https://www.reddit.com/r/MMA.json');
+//
+// 		theRequest.onload = function() {
+// 		// after the onload event occurs, 'responseText' contains complete server response
+// 			if (theRequest.status >= 200 && theRequest.status < 400) {
+// 				console.log("Request was a success");
+// 				var ourData = JSON.parse(theRequest.responseText);
+// 				console.log(ourData.data.children);
+// 				var allChildren = ourData.data.children;
+// 				displayPostTitles(allChildren);
+// 				// this above will get the title of the first post
+// 				// on the reddit/r/mma subreddit
+// 			} else {
+// 				console.log("Something went wrong");
+// 			}
+// 		}
+// 		console.log(theRequest)
+// 		theRequest.send();
+// 	});
+//
+// 	function displayPostTitles(allPosts) {
+// 		var content = document.getElementById("content");
+// 			posts = allPosts;
+// 			postNumber = allPosts.length;
+// 		for (var x = 0; x < 5; x += 1) {
+// 			var divPost = document.createElement("div");
+// 				postLink = document.createElement("a");
+// 			content.appendChild(divPost);
+// 			postLink.href = allPosts[x].data.url;
+// 			postLink.innerHTML = allPosts[x].data.title;
+// 			divPost.appendChild(postLink);
+// 		}
+// 	}
+//
+// })();
 
 
 // Function to create most popular
@@ -57,10 +57,10 @@ function showSubs(subArray) {
 				linkBox = document.createElement("div");
 
 		subLinks.className = "popSubs";
-		subLinks.innerHTML = this[i];
+		subLinks.innerHTML = popularSubs[i];
 		subDivs.className = "popDivs";
 
-		subLinks.href = "https://www.reddit.com/r/" + this[i]
+		subLinks.href = "https://www.reddit.com/r/" + popularSubs[i]
 
 		subs.appendChild(subDivs);
 		subDivs.appendChild(subLinks);
@@ -68,9 +68,12 @@ function showSubs(subArray) {
 		// Everything below this must be refactored
 		// this function is already way too bloated
 		linkBox.className = "boxOfLinks";
+		linkBox.id = popularSubs[i];
 		subDivs.appendChild(linkBox);
 
-		popRequest(this[i]);
+		popRequest(popularSubs[i]);
+		// this popRequest function brings back the top 5 links in the iterations subreddit
+		// can this be changed with recursion?
 
 	}
 }
@@ -86,10 +89,22 @@ function popRequest(sub) {
 	popSubRequest.onload = function() {
 		if (popSubRequest.status >= 200 && popSubRequest.status < 400) {
 			var popSubData = JSON.parse(popSubRequest.responseText);
-				// console.log(popSubData.data.children[0]);
-				for (var n = 0; n < 5; n +=1 ) {
-					console.log(popSubData.data.children[n]);
-				}
+			// console.log(popSubData);
+					for (var n = 0; n < 5; n += 1 ) {
+						// console.log(popSubData.data.children[n].data.subreddit);
+						// console.log(popSubData.data.children[n].data.title);
+						var linkPost = document.createElement("a");
+								linkDiv = document.createElement("div");
+								findIdDiv = document.getElementById(popSubData.data.children[n].data.subreddit);
+
+
+						linkPost.innerHTML = popSubData.data.children[n].data.title;
+						linkPost.href = 'www.reddit.com' + popSubData.data.children[n].data.permalink;
+						linkDiv.appendChild(linkPost);
+						// console.log(findIdDiv);
+						console.log(popSubData.data.children[n].data);
+						findIdDiv.appendChild(linkDiv);
+					}
 		} else {
 			console.log("Request was messed up");
 		}
@@ -98,34 +113,7 @@ function popRequest(sub) {
 	popSubRequest.send();
 }
 
-// function populateFive(subName) {
-// 	var
-// }
 
-// This bind method attaches the showSubs function to the
-// popularSubs array.
-var subArray = showSubs.bind(popularSubs);
-
-// This function is called and the popularSubs is passed in
-// an argument that points to an array of strings
-subArray(popularSubs);
-
-
-
+showSubs(popularSubs);
 
 })();
-
-
-
-function requestSubReddit(sub) {
-		var redditRequest = new XMLHttpRequest();
-		redditRequest.open('GET', 'https://www.reddit.com/r/' + sub + '.json');
-
-		// redditRequest.onload = function() {
-		// 	var requestData = JSON.parse(redditRequest.responseText);
-		// 	console.log(redditRequest.status)
-		// }
-
-		console.log(redditRequest);
-		redditRequest.send();
-	}
