7338cfdbb94d83b0b30283b0d4288bd38fb50824
Danny Kim <dkim7474@gmail.com>
2017-01-11 16:38:29 -0500

    removing unneeded commments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Wrapped in IIFE to make more modular and not to pollute global namespace
 (function() {
     var popularSubs = ["AskReddit", "funny", "todayilearned", "science", "pics", "worldnews", "IAmA", "gaming", "videos", "movies"];
     	subs = document.getElementById("subs");
@@ -20,21 +19,15 @@
             subs.appendChild(subDivs);
             subDivs.appendChild(subLinks);
 
-            // Everything below this must be refactored
-            // this function is already way too bloated
             linkBox.className = "boxOfLinks";
             linkBox.id = popularSubs[i];
             subDivs.appendChild(linkBox);
 
             popRequest(popularSubs[i]);
-            // this popRequest function brings back the top 5 links on each popularSubs iteration pertaining
-            // to that subreddit. Can this be changed with recursion?
 
         }
     }
 
-    // create a function that will send an ajax request to the .json subreddit
-    // that will bring back the links and place them in their correct div
     function popRequest(sub) {
         var popSubRequest = new XMLHttpRequest();
         popSubRequest.open('GET', 'https://www.reddit.com/r/' + sub + '.json');
@@ -43,16 +36,9 @@
             if (popSubRequest.status >= 200 && popSubRequest.status < 400) {
                 var popSubData = JSON.parse(popSubRequest.responseText);
                 for (var n = 0; n < 5; n += 1) {
-                    /*
-                     These were used to see what type of can be fetched from the returned object
-                     console.log(popSubData.data.children[n].data.subreddit);
-                     console.log(popSubData.data.children[n].data.title);
-                     console.log(popSubData.data.children[n].data);
-                    */
                     var linkPost = document.createElement("a");
                     linkDiv = document.createElement("div");
                     findIdDiv = document.getElementById(popSubData.data.children[n].data.subreddit);
-
 
                     linkPost.innerHTML = popSubData.data.children[n].data.title;
                     linkPost.href = popSubData.data.children[n].data.url;
